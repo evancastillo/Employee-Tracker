b2f37bb84a73fdefbc3fa56c5af3845fb4c2ab51
evancastillo <evancastillo1992@gmail.com>
2021-06-10 23:37:09 -0700

    added tables to sql

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3001,
+    user: 'root',
+    password: 'Evanwow1992',
+    database: 'employees'
+});
+
+const messagePrompts = {
+    viewAllDepartments: "View all Departments",
+    viewAllRoles: "View all Roles",
+    viewAllEmployees: "View all Employees",
+    addDepartment: "Add a Department",
+    addRoll: "Add a Role",
+    addEmployee: "Add an Employee",
+    updateRole: "Update an Employee Role",
+    exit: "Exit"
+};
