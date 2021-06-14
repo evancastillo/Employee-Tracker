bee6028744fb1dc18ce709a35c88da86d3f778bf
evancastillo <evancastillo1992@gmail.com>
2021-06-13 21:07:12 -0700

    adding all files

---
+++ db/seeds.sql
@@ -0,0 +1,34 @@
+INSERT INTO department (name)
+  VALUES ('Engineering'),
+         ('Finance'),
+         ('IT'),
+         ('Legal'),
+         ('Sales'),
+         ('CEO');
+
+INSERT INTO role (title, salary, department_id)
+  VALUES ('Salesperson', 75000, 5),
+         ('Customer Support', 55000, 5),
+         ('Order Processor', 55000, 5),
+         ('Attorney', 100000, 4),
+         ('Accountant', 85000, 2),
+         ('Auditor', 75000, 2),
+         ('IT Coordinator', 125000, 3),
+         ('Network Technicial', 70000, 3),
+         ('IT Technicial', 50000, 3),
+         ('Senior Software Engineer', 175000, 1),
+         ('Software Engineer', 120000, 1),
+         ('Quality Assurance Tester', 75000, 1),
+         ('CEO', 100000, 6);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+  VALUES ('Walt', 'Disney', 12, NULL),
+         ('Mickey', 'Mouse', 10, 1),
+         ('Minnie', 'Mouse', 1, 1),
+         ('Donald', 'Duck', 7, 2),
+         ('Buzz', 'Lightyear', 5, 1),
+         ('Michael', 'Jordan', 8, 4),
+         ('Barry', 'Bonds', 4, 1),
+         ('Tim', 'Lincecum', 2, 3),
+         ('Bruce', 'Wayne', 6, 5),
+         ('Peter', 'Griffin', 9, 4);
--- server.js
+++ server.js
@@ -1,22 +1,107 @@
-const mysql = require('mysql2');
 const inquirer = require('inquirer');
-const cTable = require('console.table');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+const { Departments, Roles, Employees } = require('./lib');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3001,
-    user: 'root',
-    password: 'Evanwow1992',
-    database: 'employees'
-});
+let db;
 
-const messagePrompts = {
-    viewAllDepartments: "View all Departments",
-    viewAllRoles: "View all Roles",
-    viewAllEmployees: "View all Employees",
-    addDepartment: "Add a Department",
-    addRoll: "Add a Role",
-    addEmployee: "Add an Employee",
-    updateRole: "Update an Employee Role",
-    exit: "Exit"
-};
+const mainPrompts = [
+  {
+    type: 'list',
+    name: 'action',
+    loop: false,
+    message: 'Home Menu',
+    loop: false,
+    pageSize: 20,
+    choices:
+      [
+        'View employees',
+        'Add an employee',
+        'Update an employee',
+        'Delete an employee',
+        'View departments',
+        'View department budget',
+        'Add a department',
+        'Delete a department',
+        'View roles',
+        'Add a role',
+        'Delete a role',
+        'Quit',
+      ]
+  }
+];
+
+function mainMenu() {
+  console.log('');
+  inquirer.prompt(mainPrompts)
+    .then(actionData => {
+      switch (actionData.action) {
+        case 'View employees':
+          Employees.viewEmployees(db).then(mainMenu);  // return to the action menu
+          break;
+
+        case 'Add an employee':
+          Employees.addEmployee(db).then(mainMenu);  // return to the action menu
+          break;
+
+        case 'Update an employee':
+          Employees.updateEmployee(db).then(mainMenu);  // return to the action menu
+          break;
+
+        case 'Delete an employee':
+          Employees.deleteEmployee(db).then(mainMenu);  // return to the action menu
+          break;
+
+        case 'View departments':
+          Departments.viewDepartments(db).then(mainMenu);  // return to the action menu
+          break;
+
+        case 'View department budget':
+          Departments.viewBudget(db).then(mainMenu);  // return to the action menu
+          break;
+
+        case 'Add a department':
+          Departments.addDepartment(db).then(mainMenu);  // return to the action menu
+          break;
+
+        case 'Delete a department':
+          Departments.deleteDepartment(db).then(mainMenu);  // return to the action menu
+          break;
+
+        case 'View roles':
+          Roles.viewRoles(db).then(mainMenu);  // return to the action menu
+          break;
+
+        case 'Add a role':
+          Roles.addRole(db).then(mainMenu);  // return to the action menu
+          break;
+
+        case 'Delete a role':
+          Roles.deleteRole(db).then(mainMenu);  // return to the action menu
+          break;
+
+        default:
+          console.log('\nHave a nice day!\n');
+          process.exit(0);
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+
+
+mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: 'employees'
+})
+  .then(conn => {
+    db = conn;
+    // console.log('Connected to employees database');
+    console.clear();
+    console.log(`
+   EMPLOYEE TRACKER`);
+    mainMenu();
+  });
